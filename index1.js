34f581e3a7d8817a9757d074482009c42b0dfe52
Ersan Samjaya <fernandoersan28@gmail.com>
2021-06-09 16:52:31 +0700

    Tugas 18 JS
    Tugasn ini saya kumpulkan untuk NIOMIC

---
+++ index1.js
@@ -0,0 +1,5 @@
+function Slice(){
+  var lorem = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.";
+  console.log(lorem.slice(8,24))
+}
+Slice()
